80aded29c72883ff6ea870835c15570002471dda
_antisocial_squirrel_ <belkinandrej924@gmail.com>
2022-05-24 08:42:44 +0300

    complete revision

--- main.cpp
+++ main.cpp
@@ -9,14 +9,29 @@ using namespace std;
 Filo<int> filo(3);
 CheckBrackets<char> cb;
 
+void task1()
+{
+    auto graph = new Graph();
+    int r = graph->buildTreeBFS(3);
+    auto r1 = graph->searchBFS(15);
+    auto r2 = graph->searchBFS(16);
 
+    auto r3 = graph->searchDFS(15);
+    auto r4 = graph->searchDFS(16);
 
+    graph->BFS();
+    graph->DFS();
+}
 
+void task2()
+{
+    int array[7] = {1, 4, 6, 7, 11, 36, 41};
+    pair<int, int> test = binSearch(array, 7, 41);
+    cout << test;
+}
 
 void task3()
 {
-
-
     cout << "push_back 1 -> " << filo.push_back(1) << endl;
     cout << "push_back 2 -> " << filo.push_back(2) << endl;
     cout << "push_back 3 -> " << filo.push_back(3) << endl;
@@ -38,25 +53,16 @@ void task5()
 
 }
 
-int array[7] = {1, 4, 6, 7, 11, 36, 41};
+
 
 int main() {
-    task3();
-    task5();
+    task1();
     cout << endl;
-    pair<int, int> test = binSearch(array, 7, 41);
-    cout << test;
-
-    auto graph = new Graph();
-    int r = graph->buildTreeBFS(3);
-    auto r1 = graph->searchBFS(15);
-    auto r2 = graph->searchBFS(16);
-
-    auto r3 = graph->searchDFS(15);
-    auto r4 = graph->searchDFS(16);
-
-    graph->BFS();
-    graph->DFS();
+    task2();
+    cout << endl;
+    task3();
+    cout << endl;
+    task5();
 
     return 0;
 }
